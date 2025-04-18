ea20a40a9755efc5039e05ea3b5fa33e96791199
gbotas <gbotas@uchicago.edu>
2025-04-18 22:53:08 +0000

    firstcommit

--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require "dotenv/load"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  exchange_rate_url= "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
+
+  raw_response= HTTP.get(exchange_rate_url).to_s
+
+  parsed_response= JSON.parse(raw_response)
+
+  @currencies = parsed_response.fetch("currencies").keys
+
+  erb(:homepage)
 end
+
+get("/:base_currency") do
+
+  exchange_rate_url= "https://api.exchangerate.host/list?access_key=" + ENV.fetch("EXCHANGE_RATE_KEY")
+
+  raw_response= HTTP.get(exchange_rate_url).to_s
+
+  parsed_response= JSON.parse(raw_response)
+
+  @currencies = parsed_response.fetch("currencies").keys
+  
+  @base_currency= params.fetch("base_currency").to_s
+
+  erb(:flexible)
+end 
